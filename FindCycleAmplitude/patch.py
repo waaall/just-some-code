8eac18f7e170dd7ec346c447115a77fc87589060
waaall <wallphysics@gmail.com>
2021-04-07 21:45:10 +0800

    just new features on findCycleAmp problem

---
+++ FindCycleAmplitude/patch.py
@@ -0,0 +1,190 @@
+##=========================================================
+##=======                 问题描述                 =========
+##=========================================================
+
+## 有很多个excel表格，现仅演示一个，表格有两列数据，每列单独处理
+## 每列数据是周期波动信号，周期已知，但信号不稳定，要求得每个周期的平均振幅
+## 此版本不同之处在于加入了一些机制，比如多文件夹并行处理、残缺周期分情况处理等
+
+##=========================================================
+##=======                   常量                   =========
+##=========================================================
+
+# import xlrd,openpyxl              #xlrd新版不支持xlsx格式，需要再安装openpyxl
+import os
+import numpy as np
+import pandas as pd
+# import matplotlib.pyplot as plt   #若用python画图则import
+
+WorkFolderList = ['1', '5', '25', '100', '500']
+WorkFolderName = "HZ-width and length"                  #数据所在文件夹
+
+#数据文件名//注意，所有文件名都不带格式，最后再添加
+InputFileList = ['30.8', '31.2', '31.5', '50.8', '51.2', '51.5', '70.8', '71.2', '71.5']        
+InputFormat = ".xlsx"                                   #数据文件格式
+OutSubName = ['first', 'second']                        #两列数据的名字，也作为输出文件的部分名字
+OutFormat = '.txt'                                      #最终数据输出文件格式
+ECNlist = [250, 100, 20, 20, 20]                        #一个周期数据数量Each Cycle Number
+
+
+##=========================================================
+##=======                   函数                   =========
+##=========================================================
+
+#==========================对多个文件夹批量输出===========================
+def allFolder(FolderIndexList):
+    for Index in FolderIndexList:
+        eachFolder(Index)
+
+
+#======================对每个文件夹内多个文件批量输出=======================
+def eachFolder(FolderIndex):
+    for x in InputFileList:
+        InputFile = f"{WorkFolderList[FolderIndex]}-0.{x}"
+        eachFile(InputFile, FolderIndex)
+
+
+#=========================对每个Excel文件操作输出==========================
+def eachFile(InputFile, FolderIndex):
+    WorkFolder = WorkFolderList[FolderIndex] + WorkFolderName
+    # 把数据读入内存，格式是DataFrame，矩阵形式，两列数据分别叫做first、second
+    InitMatrics = pd.read_excel(f"{WorkFolder}/{InputFile}{InputFormat}", 
+                                engine='openpyxl', header=None, names=OutSubName, usecols=[0,1])
+    
+    os.makedirs(f"{WorkFolder}/{InputFile}", exist_ok=True)
+
+    #每列数据计算保存一份
+    for col in OutSubName:
+        #这列数据转为list，然后调用函数，找出所有幅值（amplitude），并保存
+        splitAndFind(InitMatrics[col].tolist(), f"{WorkFolder}/{InputFile}/{col}", FolderIndex)
+        findAllAmp(InitMatrics[col].tolist(), f"{WorkFolder}/{InputFile}/{col}_不求平均值")
+
+
+#============最核心的函数，首先按照周期分割数据，再找到平均Amp并保存============
+def splitAndFind(List, OutFile, ECNindex):
+    ECN = ECNlist[ECNindex]
+    TotalLength = len(List)                             #list总长
+    CycleNum = int(TotalLength / ECN + 1)               #总周期数
+    ShiftNum = TotalLength - int(TotalLength / ECN) * ECN
+
+    if ShiftNum <= 5:           # 若几乎没有数据缺失或冗余，则不需要处理这个残缺周期
+        AmpList = []            
+        ANList = []
+    else:
+        AmpList = [findAveAmp(List[0 : ShiftNum])[0]]   #这个list就是每个周期的平均幅值
+        ANList = [findAveAmp(List[0 : ShiftNum])[1]]    #Amp Num list存放每个周期幅值出现次数
+
+    for x in range(1,CycleNum):
+        Head = ShiftNum + ECN * (x-1)
+        Tail = ShiftNum + ECN * x 
+        Amp, AmpNum = findAveAmp(List[Head : Tail])
+        AmpList.append(Amp)
+        ANList.append(AmpNum)
+    # 保存
+    listToTxt(AmpList, OutFile)
+    listToTxt(ANList, f"{OutFile}_每周期次数")
+
+
+#=========================找到一个周期的平均幅值=========================
+def findAveAmp(List):       #测试代码中有效率更高的函数，但这个更易读，若这个函数遇到性能瓶颈可以更换
+    AmpList = []            #存放幅值的list
+    Extreme = List[0]       #存放极值点
+    Before = List[0]        #临时值，存放前一个
+    Flag = True             #表示相邻两数的大小,后大于前为True
+    AmpNum = 0              #记录幅值出现次数
+
+    for x in List:  
+        if x - Before < 0 and Flag == True:
+            AmpList.append(abs(Before - Extreme)) 
+            Flag = False
+            Extreme = Before    #这句和上句不能换，猜测是解释器问题，无法及时将Before赋值给Extreme
+            AmpNum += 1 
+        elif x - Before > 0 and Flag == False:
+            AmpList.append(abs(Before - Extreme))
+            Flag = True
+            Extreme = Before
+            AmpNum += 1
+        Before = x
+
+    return np.mean(AmpList), AmpNum
+
+
+#=========================将list写到txt文件中=========================
+def listToTxt(List, OutFile):
+    File = open(f"{OutFile}{OutFormat}", 'w')       #w表示每次覆盖写入新文件
+    for i in range(len(List)):
+        #去除[]、逗号，换行
+        s = str(List[i]).replace('[','').replace(']','').replace(',','') +'\n'
+        File.write(s)
+    File.close()
+
+
+##=========================================================
+##=======                 测试代码                 =========
+##=========================================================
+
+# 找出所有幅值
+def findAllAmp(List, OutFile):
+    AmpList = []            #存放幅值的list
+    Extreme = List[0]       #存放极值点
+    Before = List[0]        #临时值，存放前一个
+    Flag = True             #表示相邻两数的大小,后大于前为True
+
+    # for idx, x in enumerate(List):
+    for x in List:  
+        if x - Before < 0 and Flag == True:
+            AmpList.append(Before - Extreme) 
+            Flag = False
+            Extreme = Before    #这句和上句不能换，猜测是解释器问题，无法及时将Before赋值给Extreme 
+        elif x - Before > 0 and Flag == False:
+            AmpList.append(Before - Extreme)
+            Flag = True
+            Extreme = Before
+        Before = x
+
+    listToTxt(AmpList, OutFile)
+
+#这个方法效率高一些，但是更难读，相比之下，若循环次数不多，建议用list
+def anotherfindAveAmp(List):
+    AddAmp = 0              #存放幅值的和
+    Extreme = List[0]       #存放极值点
+    Before = List[0]        #临时值，存放前一个
+    Flag = True             #表示相邻两数的大小,后大于前为True
+    Times = 0               #记录幅值出现次数
+
+    for x in List:
+        if x - Before < 0 and Flag == True:
+            AddAmp = abs(Before - Extreme) + AddAmp
+            Flag = False
+            Extreme = Before   #这句和上句不能换，猜测是解释器问题，无法及时将Before赋值给Extreme
+            Times += 1
+        elif x - Before > 0 and Flag == False:
+            AddAmp =  abs(Before - Extreme) + AddAmp
+            Flag = True
+            Extreme = Before
+            Times += 1
+        Before = x
+
+    return AddAmp / Times 
+
+
+##=========================================================
+##=======                  主代码                  =========
+##=========================================================
+
+# 这个是500HZ的，电流不全，所以单独弄，另外，500和100HZ的每周期数据太少，无法使用splitAndFind函数
+# 所以100HZ和500HZ使用specialOne函数，且需要将eachFile函数中对splitAndFind的调用注释掉
+def specialOne(FolderIndex):
+    Perfix = WorkFolderList[FolderIndex]
+    List = ['31.2', '31.5', '50.8', '51.2', '51.5', '70.8', '71.2', '71.5']
+
+    for x in List:
+        InputFile = f"{Perfix}-0.{x}"
+        eachFile(InputFile, FolderIndex)
+
+
+if __name__ == '__main__':
+    allFolder([0,1,2])
+    specialOne(3)
+    specialOne(4)   
+
