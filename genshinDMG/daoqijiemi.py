9cdfcbd0d5f0fe293ec8fe9e9e89e91f0a9077df
waaall <wallphysics@gmail.com>
2021-07-24 12:46:12 +0800

    更新了计算稻妻地图雷立方的代码，加入了确认机制，增强了代码的鲁棒性

--- genshinDMG/daoqijiemi.py
+++ genshinDMG/daoqijiemi.py
@@ -5,22 +5,30 @@ import numpy as np
 ##=========================================================
 
 #=======================核心函数：计算打击次数========================
-def Find(RoteMatrix, dimension):
-    ##输入初始状态
-    Input = input("""请将雷立方的初始状态按顺序依次输入，空格隔开：\n""")
-    InitState = np.array([int(n) for n in Input.split()])
-
+def Find(RoteMatrix, dimension, InitState):
     #初始化循环参数
     Solution = np.array([np.random.randint(0,4) for _ in range(dimension)])
     i = 0
     Zero = [0] * dimension
-    
+    WannaTry = 0
+
     #循环：结果均为4的倍数，或者循环过多次停止
-    while list(np.mod(RoteMatrix.dot(Solution)+InitState,4)) != Zero and i<2000:
+    while list(np.mod(RoteMatrix.dot(Solution)+InitState,4)) != Zero:
         Solution = np.array([np.random.randint(0,4) for _ in range(dimension)])
         i = i+1
-    
-    print(Solution)
+        if i == 5000:
+            WannaTry = int(input("未找到最优解，您若想再次尝试一下，请输入1; 若直接退出输入0："))
+            break
+
+    #若找到答案，则输出结果；若想再次尝试，则再次执行
+    if i < 5000:
+        print(f"按照您输入的顺序，打击次数分别为：\n{Solution}")
+
+    elif WannaTry == 1:
+        Find(RoteMatrix, dimension, InitState)
+
+    else:
+        print("请输入正确的数字～")
 
 #=======================四个魔方（荒海地下）========================
 def Four():
@@ -30,7 +38,10 @@ def Four():
     Dsi  = np.array([1,0,1,1])
     RoteMatrix = np.array([Dyi,Der,Dsan,Dsi]).T
 
-    Find(RoteMatrix, 4)
+    ##输入初始状态,并调用求解函数
+    Input = input("""请将雷立方的初始状态按顺序依次输入，空格隔开：\n""")
+    InitState = np.array([int(n) for n in Input.split()])
+    Find(RoteMatrix, 4, InitState)
 
 #=======================五个魔方（荒海地下）========================
 def Five():
@@ -41,20 +52,21 @@ def Five():
     Dwu  = np.array([0,0,1,0,1])
     RoteMatrix = np.array([Dyi,Der,Dsan,Dsi,Dwu]).T
 
-    print(RoteMatrix)
-
-    Find(RoteMatrix, 5)
+    ##输入初始状态,并调用求解函数
+    Input = input("""请将雷立方的初始状态按顺序依次输入，空格隔开：\n""")
+    InitState = np.array([int(n) for n in Input.split()])
+    Find(RoteMatrix, 5, InitState)
 
 
 ##=========================================================
 ##=======                  主代码                  =========
 ##=========================================================
 def main():
-    Which = int(input("""若是四个魔方，请输入4；\n若是五个魔方，请输入5：\n"""))
+    dimension = int(input("""若是四个魔方，请输入4；\n若是五个魔方，请输入5：\n"""))
     
-    if Which == 4:
+    if dimension == 4:
         Four()
-    elif Which == 5:
+    elif dimension == 5:
         Five()
     else:
         print("请输入正确的数字～")
