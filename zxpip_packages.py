baa6bd0c4af21d08cb48f95413c139f66a617370
waaall <wallphysics@gmail.com>
2026-03-06 11:33:19 +0800

    add graphviz

--- zxpip_packages.py
+++ zxpip_packages.py
@@ -13,6 +13,7 @@ class zxPackageManager:
         # 列出需要安装或更新的库
         self.required_packages = [
             'charset_normalizer',
+            'graphviz',
             'pipdeptree',
             'tqdm',
             'pypdf',
